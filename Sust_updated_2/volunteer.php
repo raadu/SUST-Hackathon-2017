9db48e7830df9208fb95ca0a9f9ddc18acd7b449
unknown <raiyad.raad@gmail.com>
2017-10-30 01:24:17 +0600

    SUST Files

---
+++ Sust_updated_2/volunteer.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    
+    
+    <title>Be a volunteer</title>
+    
+    <link href="assets/css/bootstrap.css" rel="stylesheet" />
+    <
+    <link href="assets/css/font-awesome.min.css" rel="stylesheet" />
+    
+    <link href="assets/css/font-awesome-animation.css" rel="stylesheet" />
+     
+    <link href="assets/css/prettyPhoto.css" rel="stylesheet" />
+       
+    <link href="assets/css/style.css" rel="stylesheet" />
+    
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+</head>
+<body>
+ <div class="navbar navbar-inverse navbar-fixed-top">
+       
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                
+            </div>
+            <div class="navbar-collapse collapse">
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="index.html">HOME</a></li>
+                    <li><a href="places.html">PLACES</a></li>
+                     <li><a href="Gallery.html">PACKAGES</a></li>
+                    <li><a href="details.html">BE A VOLUNTEER</a></li>
+                </ul>
+            </div>
+           
+        </div>
+    </div>
+<div class="container-fluid">
+	<div class="col-md-2"></div>
+	<div class="col-md-8">
+		<div id="form1">
+		    <h4>Sign Up as a volunteer!</h4>
+		    
+			<form>
+			<input type="text" name="Your_Name" placeholder="Your Name">
+			<input type="text" name="Your_Age" placeholder="Your Age">
+			<input type="text" placeholder="Your Area">
+
+            <datalist id="Your_Area">
+            <option value="Dhaka">
+            <option value="Sylhet">
+            <option value="Chittagong">
+            <option value="Rajshahi">
+            <option value="Barisal">
+            <option value="Mymensing">
+            </datalist>
+
+            <input type="text" placeholder="What You prefer to do?">
+
+            <datalist id="job">
+            <option value="Residence">
+            <option value="Food">
+            <option value="Guide">
+            <option value="Rescue personnel">
+            
+            </datalist>
+
+            <input type="text" name="nid" placeholder="Your NID/passport no.">
+            <input type="text" name="phone" placeholder="Your phone no.">
+            <input type="text" name="email" placeholder="Your email:"><br>
+            <input type="submit" name="submit">
+
+				
+			</form>
+		</div>
+	</div>
+	<div class="col-md-2"></div>
+</div>
+
+</body>
+</html>
